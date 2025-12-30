af7e54dffb51d7d5822da3bb9bcb493a1bbb406a
vboxuser <vboxuser@cccc.myguest.virtualbox.org>
2025-12-30 01:19:08 +0000

    improved UI

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Peter ?</h1><p>This Flask app is running inside Docker ?</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
